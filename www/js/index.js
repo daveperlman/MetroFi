e29b63d489e6784524d5e0ba0d835227eadc674d
Dave Perlman <daveperlman@gmail.com>
2014-07-12 17:26:51 +0200

    Update index.js

--- www/js/index.js
+++ www/js/index.js
@@ -131,12 +131,12 @@ var app = {
                 );
 
                 //UPDATE PAGE
-                $("#message_list").append("<li class='mf-link'><h3>:: "+e.data.title+"</h3>"+e.message+"</li>");
+                $("#message_list").append("<li><h3>:: "+e.data.title+"</h3>"+e.message+"</li>");
                 $('#message_list').listview('refresh');
 
-                $(".mf-link").on("click", function(){
-                    window.open('http://metrofi.co.za?userid='+app.userid,'_system','location=no');
-                });
+                //$(".mf-link").on("click", function(){
+                //    window.open('http://metrofi.co.za?userid='+app.userid,'_system','location=no');
+                //});
 
                 //PING BACK TO SERVER
                 _params = {};
