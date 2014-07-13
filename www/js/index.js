1d241df483c634b85e4f44d0454800181c1889cd
Dave Perlman <daveperlman@gmail.com>
2014-07-13 14:29:54 +0200

    Update index.js

--- www/js/index.js
+++ www/js/index.js
@@ -156,7 +156,7 @@ var app = {
         );
 
         //UPDATE PAGE
-        $("#message_list").append("<li><h3>:: "+e.payload.title+"</h3>"+e.message+"</li>");
+        $("#message_list").append("<li><h3>:: "+"e.payload.title"+"</h3>"+e.message+"</li>");
         $('#message_list').listview('refresh');
 
         //$(".mf-link").on("click", function(){
