cf702042c8dd05a2d5c998617eaa24954c8c9cbb
Dave Perlman <daveperlman@gmail.com>
2014-07-16 14:47:09 +0200

    Update index.js

--- www/js/index.js
+++ www/js/index.js
@@ -181,13 +181,15 @@ var app = {
         _advertparams.macaddress = app.MACAddress;
         $.get("http://metrofi.co.za/client/getadvert.php?",_advertparams,function(_advert){
            //alert(_advert[0].type+_advert[0].content);
-             if (_advert[0].type == "image") {
-             //alert(_advert[0].content);
-                 _messageblock.find(".msg-content").append("<img src='http://metrofi.co.za/client/images/"+_advert[0].content+"' style='width:100%;'");            
+             if (_advert) {
+                 if (_advert[0].type == "image") {
+                 //alert(_advert[0].content);
+                     _messageblock.find(".msg-content").append("<img src='http://metrofi.co.za/client/images/"+_advert[0].content+"' style='width:100%;'");            
+                 }
+                 if (_advert[0].url) {
+                     _url = _advert[0].url;
+                 }    
              }
-             if (_advert[0].url) {
-                 _url = _advert[0].url;
-             }    
         }).always(function(){
             _messageblock.on("click", function(){
                 window.open(_url,'_system','location=no');
