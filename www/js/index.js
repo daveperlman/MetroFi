9d8d02e09cfa21089125d62818a1ab0a12b299ac
Dave Perlman <daveperlman@gmail.com>
2014-07-16 14:06:37 +0200

    Update index.js

--- www/js/index.js
+++ www/js/index.js
@@ -182,10 +182,12 @@ var app = {
         $.get("http://metrofi.co.za/client/getadvert.php?",_advertparams,function(_advert){
            //alert(_advert[0].type+_advert[0].content);
              if (_advert[0].type == "image") {
-              alert(_advert[0].content));
+             alert(_advert[0].content));
              //    _messageblock.find(".msg-content").append("<img src='"+_advert[0].content+"' style='width:100%;'");            
              }
-             _url = _advert[0].url;
+             if (_advert[0].url) {
+                 _url = _advert[0].url;
+             }    
         }).always(function(){
             _messageblock.on("click", function(){
                 window.open(_url,'_system','location=no');
